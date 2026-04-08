25247b2cd9e857e7759c0a1a41a3d1e09cc8e9bd
Ariel Aramnia <arielaramnia@gmail.com>
2026-04-08 16:10:51 -0700

    Initial Commit

---
+++ Spend Friend New/Spend_Friend_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spend_Friend_NewApp.swift
+//  Spend Friend New
+//
+//  Created by ariel aramnia on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Spend_Friend_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
